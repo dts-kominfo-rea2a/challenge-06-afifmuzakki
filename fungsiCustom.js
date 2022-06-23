f07b03e259fd624f69b33f28c08e3e91e031db98
afifmuzakki <afifmuzakki12@gmail.com>
2022-06-23 15:50:36 +0700

    result of challenge 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,22 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let arr = [];
+  fs.readFile(file1, (err, data) => {
+    if (err) return fnCallback(err, null);
+    arr.push(JSON.parse(data).message.split(" ")[1]);
+    fs.readFile(file2, (err, data) => {
+      if (err) return fnCallback(err, null);
+      arr.push(JSON.parse(data)[0].message.split(" ")[1]);
+      fs.readFile(file3, (err, data) => {
+        if (err) return fnCallback(err, null);
+        arr.push(JSON.parse(data)[0].data.message.split(" ")[1]);
+        fnCallback(err, arr);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
